6347bb28ea37a536bb3b82da0529b32feecaaa6c
mgm1027 <mgm1027@alu.ubu.es>
2024-02-08 18:33:38 +0100

    Implementar método para la primera partición de Quicksort (Bidireccional)
    Creación del método para corregir la pregunta sobre la primera partición bidireccional de Quicksort.

--- src/main/quicksort.py
+++ src/main/quicksort.py
@@ -1,8 +1,51 @@
 import xml.etree.ElementTree as ET
 
+# Método que calcula la primera partición obtenida tras pivotar
+# bidireccionalmente con el primer elemento como pivote del algoritmo
+# de ordenación Quicksort
+# Autor: Mario García Martínez
+def quickSort_bidireccional(vector, inicio, fin):
+
+    # Copia del vector para no modificar el original
+    vectorResultado = list(vector)
+    pivote = vectorResultado[inicio]
+    # Primer puntero señala al primer elemento posterior al pivote
+    izq = inicio + 1
+    # Segundo elemento señala al último elemento del vector
+    der = fin
+    
+
+    while True:
+        # Siempre que el puntero izquierdo este señalando una posición menor o igual a la del segundo
+        
+        # Si el valor que señala el puntero de la derecha es mayor que el pivote se pasa al siguiente valor
+        while izq <= der and vectorResultado[der] >= pivote:
+            der = der - 1
+
+        # Si el valor que señala el puntero de la izquierda es menor que el pivote se pasa al siguiente valor
+        while izq <= der and vectorResultado[izq] <= pivote:
+            izq = izq + 1
+
+        # (No salen del bucle hasta encontrar dos casos que no cumplan por lo cual son adecuados para ser intercambiados)
+
+        # Si el puntero izquierdo está señalando una posición menor o igual a la del segundo 
+        if izq <= der:
+            # Se intercambian los valores a los que señalan los punteros
+            vectorResultado[izq], vectorResultado[der] = vectorResultado[der], vectorResultado[izq]
+        else:
+            # Si los punteros ya han finalizado
+            break
+
+    # Se coloca al pivote en la posición donde el puntero derecho ha finalizado (posición intermedia)
+    vectorResultado[inicio], vectorResultado[der] = vectorResultado[der], vectorResultado[inicio]
+
+    return vectorResultado, der
+
 # Pregunta de ejemplo sobre la primera llamada tras ejecutar quicksort (rellenar)
 # Autor : Mario García Martínez
 def crear_pregunta_quicksort(vector):
+
+    solucionBidireccional, finBidireccional = quickSort_bidireccional(vector, 0, (len(vector)-1) )
 
     # Creamos el elemento raíz del xml
     quiz = ET.Element("quiz")
@@ -47,7 +90,7 @@ def crear_pregunta_quicksort(vector):
         </table>
         ]]>
     """.format(''.join(['<td>{}</td>'.format(val) for val in vector]), # Visualización de vector en el enunciado
-               ''.join(['<td>{{1:NUMERICAL:%100%{}:0.1#}}</td>'.format(val) for val in sorted(vector) ]), # Solución 1 (Bidireccional)
+               ''.join(['<td>{{1:NUMERICAL:%100%{}:0.1#}}</td>'.format(val) for val in solucionBidireccional ]), # Solución 1 (Bidireccional)
                ''.join(['<td>{{1:NUMERICAL:%100%{}:0.1#}}</td>'.format(val) for val in sorted(vector) ])) # Solución 2 (Unirrecional)
 
     generalfeedback = ET.SubElement(question, "generalfeedback", format="html")
