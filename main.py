b8e3aed19c9476172d354affe173cbd8df78d4d7
mgm1027 <mgm1027@alu.ubu.es>
2024-04-17 12:08:06 +0200

    Implementación de página básica y formulario para generar bancos de preguntas.

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for,send_file, redirect
 import src.main.mergesort
 import src.main.quicksort
 
 #va a buscar en el modulo principal de nuestra app (plantillas en templates)
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'src'
 
 @app.route('/')
 def index():
-    name = None
-    list= ['e1','e2','e3']
-    return render_template('inicio.html', name = name, list = list)
+    return render_template('inicio.html')
 
-#string
-@app.route('/hello')
-@app.route('/hello/<name>')
-@app.route('/hello/<name>/<int:age>')
-def hello(name = None, age = None):
-    if name == None and age == None:
-        return 'Hola Mundo!'
-    elif age == None:
-        return f'Hola, {name}'
-    else:
-        return f'Hola, {name} tienes {age} años!'
+@app.route('/mergesort')
+def mergesort():
+    return render_template('mergesort.html')
+@app.route('/quicksort')
+def quicksort():
+    return render_template('quicksort.html')
 
-#int
-#path
+@app.route('/formularioMergesort', methods=['GET','POST'])
+def formularioMergesort():
+    if request.method == 'GET':
+        return render_template('formularioMergesort.html')
+    numero_preguntas= int(request.form.get('numero_preguntas'))
+    src.main.mergesort.generar_preguntas_mergesort(numero_preguntas)
+    return redirect(url_for('datosMergesort'))
 
-from markupsafe import escape # para poner un path que lo muestre y no lo ejecute
-@app.route('/code/<path:code>')
-def code(code):
-    return f'<code>{escape(code)}</code>'
+@app.route('/formularioQuicksort', methods=['GET','POST'])
+def formularioQuicksort():
+    if request.method == 'GET':
+        return render_template('formularioQuicksort.html')
+    numero_preguntas= int(request.form.get('numero_preguntas'))
+    src.main.quicksort.generar_preguntas_quicksort(numero_preguntas)
+    return redirect(url_for('datosQuicksort'))
+
+@app.route('/datosQuicksort')
+def datosQuicksort():
+    return render_template('datosQuicksort.html')
+@app.route('/datosMergesort')
+def datosMergesort():
+    return render_template('datosMergesort.html')
+   
+@app.errorhandler(404)
+def pagina_no_encontrada(error):
+    return redirect(url_for('index'))
+
+@app.route('/download_file/<filename>', methods=['GET'])
+def download_file(filename):
+    file_path = ''
+    if filename == 'pregunta_mergesort.xml':
+        file_path = 'pregunta_mergesort.xml'
+    elif filename == 'pregunta_quicksort.xml':
+        file_path = 'pregunta_quicksort.xml'
+
+    return send_file(file_path, as_attachment=True)
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
